78eea6f420c57479252c695032ca89c208304ea8
SergiuXx20 <sergiodanielsm@alumnos.iesgalileo.es>
2021-03-17 00:11:50 +0100

    JAVASCRIPT

---
+++ app.js
@@ -0,0 +1,42 @@
+//document.write("<h1>Hola mundo</h1>");
+//tipos de datos
+"Hola marica" //string
+
+//boolean
+true
+false
+
+//number
+10
+-10
+10.3
+-10.3
+
+//array
+["Abraham", "Pepe", "Gustavo Hitler"]
+[true, false, true, false]
+[1, 2, 3]
+
+// objects
+"Manuel"
+2000
+14
+true
+
+//Ejemplo de objetos
+console.log("this");
+{
+    "username": "Manuel",
+    "score": 2000,
+    "hours": 14,
+    "professional": true
+    "friends": ["Juan","Miguel","Marcos"]
+}
+
+{
+    "username": "Hitler",
+    "score": 300000,
+    "hours": 4657,
+    "professional": true
+    "friends": ["Mussolinni","Franco","Pedro Sanchez"]
+}
---
+++ prueba.js
@@ -0,0 +1,9 @@
+//Mostrar por consola un objeto
+//console.log({"username":"Hitler","score":300000});
+
+ 
+var variable = "Joe";
+console.log(variable)
+
+
+
